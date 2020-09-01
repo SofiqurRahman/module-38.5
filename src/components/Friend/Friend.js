8a98152e58cb43056f559249528c516b5ec179d0
SofiqurRahman <sofiqurr849@gmail.com>
2020-09-01 13:30:35 -0700

    first commit

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+const Friend = (props) => {
+  const{name,email,id}=props.friend;
+
+  const history=useHistory();
+  const handleClick=(friendId)=>{
+    const url=`/friend/${friendId}`;
+    history.push(url);
+  }
+  
+  const friendStyle={
+    border: '1px solid purple',
+    margin: '20px',
+    padding: '20px',
+    borderRadius: '20px'
+  }
+  return (
+    <div style={friendStyle}>
+      <h2>Name: {name}</h2>
+      <p>email: {email}</p>
+      <Link to={`/friend/${id}`}>Show detail of {id}</Link>
+      <button onClick={()=>handleClick(id)}>click me</button>
+      {/* <Link to={`/friend/${id}`}>
+        <button>Show detail of {id}</button> 
+      </Link> */}
+    </div>
+  );
+};
+
+export default Friend;
